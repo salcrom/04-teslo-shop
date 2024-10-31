25f100e685100e2a154745a69f4d335bf72b2786
salcrom <seya.28@gmail.com>
2024-10-31 18:38:21 +0100

    Diseño parte 1 listo

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,10 +1,25 @@
 // Página para mostrar las categorías de hombres, mujeres y niños por id
 
+import { notFound } from "next/navigation";
 
-export default function() {
+
+interface Props{
+  params:{
+    id: string;
+  }
+}
+
+export default function({ params }:Props ) {
+
+  const { id } = params;
+
+  if ( id === 'kids' ){
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Category Page { id }</h1>
     </div>
   );
 }
