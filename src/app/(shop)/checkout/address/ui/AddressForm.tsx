6034fcc634603e331b2374108b73813aceac7e8c
salcrom <seya.28@gmail.com>
2024-11-29 21:36:24 +0100

    fix warning listo

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -50,7 +50,7 @@ export const AddressForm = ({countries, userStoredAddress = {} }:Props) => {
         if( address.firstName ){
             reset(address)
         }
-    }, [])
+    }, [address, reset])
     
 
     const onSubmit = async( data: FormInputs ) => {
--- src/components/product/stock-label/StockLabel.tsx
+++ src/components/product/stock-label/StockLabel.tsx
@@ -16,14 +16,15 @@ export const StockLabel = ( { slug }: Props ) => {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        const getStock = async() => {
+            const inStock = await getStockBySlug(slug);
+            setStock( inStock );
+            setIsLoading(false);
+        };
+
         getStock( );
-    }, [])
+    }, [slug])
     
-    const getStock = async() => {
-        const inStock = await getStockBySlug(slug);
-        setStock( inStock );
-        setIsLoading(false);
-    }
 
     return (
         <>
